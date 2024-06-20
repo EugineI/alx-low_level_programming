9b5400e5f76dcfe0a7184732d99aa0eb5aa4a77a
EugineI <eugineindimuli@gmail.com>
2024-06-20 03:36:01 -0700

    using put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+/*This is the main funtion of our code*/
 int main()
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
