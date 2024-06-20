00f3415801a9fd287988d2aac41068a2b4227ce9
EugineI <eugineindimuli@gmail.com>
2024-06-20 04:35:13 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - is the start point of the program.
+ *
+ * Description: The funtion uses printf to display a message.
+ *
+ * Return: the result is always 0 to show success.
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
