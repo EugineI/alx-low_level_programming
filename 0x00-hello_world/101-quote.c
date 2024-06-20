0187e942197d14c3bd58432b8573df18a56020ed
EugineI <eugineindimuli@gmail.com>
2024-06-20 12:31:32 -0700

    write function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<unistd.h>
+/*
+ * main: this is the start point of the program
+ *
+ *Description: displaying of a message without using a funtion
+ *
+ * Return: result is 1 to show error
+ */ 
+int main(void)
+{
+	char message[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, 59);
+	return (1);
+}
