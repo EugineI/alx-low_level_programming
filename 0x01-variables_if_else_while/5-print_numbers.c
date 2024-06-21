74e84339573da5a0b0057e4ee0a964753260a5b4
EugineI <eugineindimuli@gmail.com>
2024-06-21 14:29:53 -0700

    for loop for numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ *main - is the entry point of the program.
+ *
+ * Description:printing all the digits of base 10 using for loop
+ *
+ * Return: the result is always 0 to show success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
