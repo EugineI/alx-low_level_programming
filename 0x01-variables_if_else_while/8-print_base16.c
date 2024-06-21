3127a2d45a63b1e115a542e86a353d615d92708d
EugineI <eugineindimuli@gmail.com>
2024-06-21 15:23:03 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - the start point of the program
+ *
+ * Description: The function uses for loop and putchar to display hexadecimals
+ *
+ * Return: the result is 0 to indicate success.
+ */
+int main(void)
+{
+	char hexa[] = "0123456789ABCDEF";
+	int i;
+
+	for (i = 0; i <= 15; i++)
+	{
+		putchar(hexa[i]);
+	}
+	putchar('\n');
+	return (0);
+}
