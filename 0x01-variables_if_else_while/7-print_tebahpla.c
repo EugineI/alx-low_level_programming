6d670829f1574a56b82c9832492cb8d0b820f6b5
EugineI <eugineindimuli@gmail.com>
2024-06-21 14:57:42 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - the start point of the program
+ *
+ * Description: The function uses for loop and putchar to display the alphabets
+ *
+ * Return: the result is 0 to indicate success.
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
