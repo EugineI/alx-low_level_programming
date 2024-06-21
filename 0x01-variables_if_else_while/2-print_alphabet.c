1725cdffefc838a0612855cfd183dce84ee782d9
EugineI <eugineindimuli@gmail.com>
2024-06-21 12:19:26 -0700

    for loops

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,12 +5,12 @@
  * Description: The function uses for loop and putchar to display the alphabets
  *
  * Return: the result is 0 to indicate success.
- */ 
-int main()
+ */
+int main(void)
 {
 	char ch; 
 
-	for (ch = 'a'; ch <= 'z';ch++)
+	for (ch = 'a'; ch <= 'z';ch ++)
 	{
 		putchar(ch);
 	}
