417d5e9a5b94a3e56a36672bb06d46c35ec46459
EugineI <eugineindimuli@gmail.com>
2024-06-25 02:21:59 -0700

    printing putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - the entry point
+ *
+ * Description: print putchar statement.
+ *
+ * Return: 0 for success.
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
