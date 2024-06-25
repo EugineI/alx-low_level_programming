fed8bba16836daa8501f496446f99e52e494ca49
EugineI <eugineindimuli@gmail.com>
2024-06-25 15:21:03 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - is the open function
+ *
+ * Description: printing the last digit
+ *
+ * @n: is the check character
+ *
+ * Return: the last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
