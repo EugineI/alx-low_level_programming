5074a91b74ad3e2e210a0a6f09db1a2e3f30202f
EugineI <eugineindimuli@gmail.com>
2024-06-26 05:24:03 -0700

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - takes two integers and prints the sum
+ *
+ * Description: the function culculates the sum;
+ *
+ * @a: first number
+ *
+ * @b: second number
+ *
+ * Return: the result of the sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
