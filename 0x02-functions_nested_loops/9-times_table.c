5d7a2121afc1438346d6c9eb399831b97e839b6a
EugineI <eugineindimuli@gmail.com>
2024-06-26 05:01:22 -0700

    aligning

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,9 +22,13 @@ void times_table(void)
 				_putchar((product / 10) + '0');
 				_putchar((product % 10) + '0');
 			}
-			else
+			else if (product < 10 && j != 0)
 			{
 				_putchar(' ');
+				_putchar((product) + '0');
+			}
+			else
+			{
 				_putchar((product) + '0');
 			}
 			if (j != 9)
