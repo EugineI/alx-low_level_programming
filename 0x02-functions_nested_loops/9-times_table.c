058add4a8e5b271b332488eb9e853042657749d7
EugineI <eugineindimuli@gmail.com>
2024-06-26 04:46:58 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - the entry point of the program
+ *
+ * Description: function creates the 9times table
+ *
+ * Return: returns no value
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int product;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			product = i * j;
+			if (product > 9)
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar((product) + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
