dce2c591a83e89cc97c79e01786e8fa7eaf412f8
EugineI <eugineindimuli@gmail.com>
2024-07-01 13:26:07 -0700

    second half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts_half - prints half of the string
+ *
+ * @str: string character
+ *
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int i;
+	int length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	if ((length % 2) == 0)
+	{
+		for (i = length / 2; i < length; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+		for (i = (length - 1) / 2; i < length; i++)
+		{
+			_putchar(str[i]);
+		}
+	_putchar('\n');
+}
