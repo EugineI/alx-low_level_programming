cdf4033bb8761c8f66e3451ca1a1dd8cfb410b4e
EugineI <eugineindimuli@gmail.com>
2024-07-03 02:05:06 -0700

    comparing strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ *
+ * @dest: destination
+ *
+ * @n: the size variable
+ *
+ * @src: source
+ *
+ * Return: pointer to dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *new = dest;
+	int i;
+
+	while (*new != '\0')
+	{
+		new++;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		*new = src[i];
+	}
+	*new = '\0';
+	return (dest);
+
+}
