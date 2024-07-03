f4c26d3f6451eb7191069c827872a9ac8b8b0998
EugineI <eugineindimuli@gmail.com>
2024-07-03 11:52:48 -0700

    several changes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,6 +10,10 @@ char *cap_string(char *s)
 {
 	int i = 0;
 
+	if (s[i] >= 'a' && s[i] <= 'z')
+	{
+		s[i] = s[i] - 'a' + 'A';
+	}
 	while (s[i] != '\0')
 	{
 		if (s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n'
