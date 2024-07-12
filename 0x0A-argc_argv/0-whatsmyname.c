c4efc187d8d16cc39f32d4165c8c16057e71667a
EugineI <eugineindimuli@gmail.com>
2024-07-12 04:30:41 -0700

    program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "main.h"
-/*
+/**
  * main - entry point
  *
  * @argc: argument count
@@ -9,9 +9,9 @@
  *
  * Return: always 0
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
