6f6c6581acec5ef3329a25975d44249535e579b7
EugineI <eugineindimuli@gmail.com>
2024-07-18 02:21:10 -0700

    malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include"main.h"
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * malloc_chesked - allocates memory using malloc
+ *
+ * @b: size
+ *
+ * Return: pointer to allocated address
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *str = malloc(b);
+
+	if (str == NULL)
+	{
+		exit(98);
+	}
+	return (str);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include"main.h"
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+/**
+ * string_nconcat - concatenates two strings
+ *
+ * @s1: first string
+ *
+ * @s2: second string
+ *
+ * @n: number of bytes
+ *
+ * Return: NULL or newly located space in memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	unsigned int i;
+	unsigned int j;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (n >= strlen(s2))
+	{
+		n = strlen(s2);
+	}
+	str = malloc(strlen(s1) + n + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < strlen(s1); i++)
+	{
+		str[i] = s1[i];
+	}
+	for (j = 0; j < n; j++)
+	{
+		str[i + j] = s2[j];
+	}
+	str[i + j] = '\0';
+	return (str);
+}
+
