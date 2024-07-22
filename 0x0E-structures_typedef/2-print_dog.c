90255dbff23140a8537c7b0cfd0947831db0a551
EugineI <eugineindimuli@gmail.com>
2024-07-22 04:59:30 -0700

    another attempt

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,6 +8,7 @@
  */
 void print_dog(struct dog *d)
 {
+	d = malloc(sizeof(struct dog));
 	if (d == NULL)
 	{
 		return;
@@ -18,17 +19,16 @@ void print_dog(struct dog *d)
 	}
 	else
 		printf("Name: %s\n", d->name);
+	if (d->owner == NULL)
+	{
+		printf("Owner: nil\n");
+	}
+	else
+			printf("Owner: %s\n", d->owner);
 	if (d->age < 0 || d->age == 0)
 	{
 		printf("Age: nil\n");
 	}
 	else
 		printf("Age: %.6f\n", d->age);
-	if (d->owner == NULL)
-	{
-		printf("Owner: nil\n");
-	}
-	else
-		printf("Owner: %s\n", d->owner);
-
 }
