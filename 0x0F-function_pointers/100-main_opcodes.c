66b79c14f07b253bed2949e82c7d43fa27158557
EugineI <eugineindimuli@gmail.com>
2024-07-24 11:33:35 -0700

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, char *argv[]);
+/**
+ * print_opcodes - return opcode for the main.
+ * @bytes: bytes
+ * Return: opcode
+ */
+void print_opcodes(int bytes)
+{
+	void (*main_ptr)(void) = &main;
+	unsigned char *ptr = (unsigned char *)main_ptr;
+	int i;
+
+	for (i = 0; i < bytes; ++i)
+	{
+		printf("%02x", *ptr++);
+		if (i != bytes - 1)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+}
+/**
+ * main - entry
+ * @argc: count
+ * @argv: vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc  != 2)
+	{
+		fprintf(stderr, "Error\n");
+		return (1);
+	}
+	int bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		fprintf(stderr, "Error\n");
+		return (2);
+	}
+	print_opcodes(bytes);
+	return (0);
+}
