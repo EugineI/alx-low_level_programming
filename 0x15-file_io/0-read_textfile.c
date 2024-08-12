abbdbc1dabe36884e8f16e13da7876df83bf11bc
EugineI <eugineindimuli@gmail.com>
2024-08-12 02:30:18 -0700

    opening and writting on a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it
+ * @filename: name of file
+ * @letters: number of letters
+ * Return: number of letters:
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t reads, written;
+	char *buffer;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	reads = read(fd, buffer, letters);
+	if (reads < 0)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+	written = write(2, buffer, reads);
+	if (written < 0 || written != reads)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+	close(fd);
+	free(buffer);
+
+	return (written);
+}
