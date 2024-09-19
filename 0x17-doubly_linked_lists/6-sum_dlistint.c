54c598485eabe167e42f56ce7c2bf1301c603171
EugineI <eugineindimuli@gmail.com>
2024-09-19 19:23:16 +0300

    delete

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include"lists.h"
+#include<stdio.h>
+/**
+ * sum_distint - sums up all the element
+ * @head - pointer to the head
+ * Return: 0 or sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	 while (head != NULL)
+	 {
+		 sum += head->n;
+		 head = head->next;
+	 }
+	 return (sum);
+}
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,38 @@
+#include"lists.h"
+#include<stdlib.h>
+/**
+ * delete_dnode_at_index - deletes a node
+ * @head: pointer to head
+ * @index: index of node to be deleted
+ * Return: 1 for success and -1 for failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = temp->next;
+		if (temp->next != NULL)
+			temp->next->prev = NULL;
+		free(temp);
+		return(1);
+	}
+	while (temp && i < index)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if (temp == NULL)
+		return (-1);
+
+	if (temp->next != NULL)
+		temp->next->prev = temp->prev;
+	if (temp->prev != NULL)
+		temp->prev->next = temp->next;
+	free(temp);
+	return(1);
+}
