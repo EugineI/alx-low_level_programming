9b623991e8cd71852c8190e93c5b69872c077bfb
EugineI <eugineindimuli@gmail.com>
2024-09-19 15:17:37 +0300

    double links

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include"lists.h"
+#include<stdio.h>
+/**
+ * dlistint_len - length of the list
+ * @h: pointer to head
+ * Return: length of the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return(count);
+}
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include"lists.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * add_dnodeint - adds a node at the beginning
+ * @head: pointer to head
+ * @n: node data
+ * Return: newnode or null
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return(NULL);
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
+	*head = new_node;
+	return(new_node);
+}
